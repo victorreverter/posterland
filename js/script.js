dc75df471a65c2e1e519e5e45281b76961d64cd3
victorreverter <victormanuel.cuba@gmail.com>
2022-03-01 05:30:18 +0100

    Update Home 4 - Animation

--- js/script.js
+++ js/script.js
@@ -1,54 +1,33 @@
-// Animate Dark / Light Mode
+// Animate Home
 
+TOP_LEAVES = document.querySelector('#top_leaves');
+LEFT_LEAF = document.querySelector('#left_leaf');
+RIGHT_LEAF = document.querySelector('#right_leaf');
+HOME_SOFA = document.querySelector('#home_sofa');
 
-// Footer Activation
+PROJECT_CONTENT = document.querySelector('#project_content');
 
-const FOOTER = document.querySelector("#footer");
-// const div = document.createElement('div');
+setTimeout(() => {
+    TOP_LEAVES.classList.remove('hideLeft');
+    // LEFT_LEAF.classList.remove('hideLeft');
 
-FOOTER.innerHTML = `<div class="container-links">
-                <a href="https://www.instagram.com/victorreverter/">Instagram</a>
-                <a href="https://www.behance.net/VictorReverter">Behance</a>
-                <a href="https://www.linkedin.com/in/victor-manuel-herrera-reverter-a9312348/">LinkedIn</a>
-            </div>
-            <div class="copyright">© Victor Reverter Devs 2021</div>`;
+    RIGHT_LEAF.classList.remove('hideRight');
+    // HOME_SOFA.classList.remove('hideBottom');
 
-// Animate Fade In Elements
-let elements;
-let windowHeight;
+    // PROJECT_CONTENT.classList.add('showElem');
+}, 400);
 
-function init () {
-    elements = document.querySelectorAll('.hidden');
-    windowHeight = window.innerHeight;
-}
+setTimeout(() => {
+    // TOP_LEAVES.classList.remove('hideLeft');
+    LEFT_LEAF.classList.remove('hideLeft');
 
-function checkPosition() {
-    for (let i = 0; i < elements.length; i++) {
-        let element = elements[i];
-        let positionFromTop = elements[i].getBoundingClientRect().top;
+    // RIGHT_LEAF.classList.remove('hideRight');
+    HOME_SOFA.classList.remove('hideBottom');
 
-        // console.log(positionFromTop);
-        console.log(positionFromTop - windowHeight);
-        // console.log(-(windowHeight + 120));
+    // PROJECT_CONTENT.classList.add('showElem');
+}, 800);
 
-        // console.log(positionFromBottom - windowHeight);
+setTimeout(() => {
+    PROJECT_CONTENT.classList.add('showElem'); 
+}, 1500);
 
-        if(positionFromTop - windowHeight <= -80) {
-            element.classList.remove('fade-out-element');
-            element.classList.add('fade-in-element');
-            element.classList.remove('hidden');
-        }
-
-        if(positionFromTop - windowHeight >= -81) {
-            element.classList.remove('fade-in-element');
-            // element.classList.add('fade-out-element');
-            element.classList.add('hidden');
-        }
-    }
-}
-
-window.addEventListener('scroll', checkPosition);
-window.addEventListener('resize', init);
-
-init();
-checkPosition();
