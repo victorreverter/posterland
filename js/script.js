03505126d70b19bcec9d0fbd48af15e63feeff08
victorreverter <victormanuel.cuba@gmail.com>
2022-03-04 05:39:54 +0100

    Update Projects 2

--- js/script.js
+++ js/script.js
@@ -31,3 +31,23 @@ setTimeout(() => {
     PROJECT_CONTENT.classList.add('showElem'); 
 }, 1500);
 
+// SLIDE animation
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("pic-item");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    // x[i].style.display = "none";
+    x[i].style.opacity = 0;
+  }
+  x[slideIndex-1].style.opacity = 1;
+}
